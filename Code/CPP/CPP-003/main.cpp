829bc70552a4a9f0a160a0074bb10699fa927c69
WPM9gvkprv-coder <wpm9gvkprv@privaterelay.appleid.com>
2026-07-11 11:38:38 +0800

    输入输出

---
+++ Code/CPP/CPP-003/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    int sum;
+    cout<<"请输入第一个数：";
+    cin>>a;
+    cout<<"请输入第二个数：";
+    cin>>b;
+    sum=a+b;
+    cout<<a<<"+"<<b<<"="<<sum<<endl;
+    return 0;
+}
