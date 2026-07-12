6aa8df9a02c36e2a4709ac0a23d803d35acd6114
WPM9gvkprv-coder <wpm9gvkprv@privaterelay.appleid.com>
2026-07-12 11:23:30 +0800

    运算符

---
+++ Code/CPP/CPP-004/main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+    double sum;
+    double diff;
+    double product;
+    double quotient;
+    double remainder;
+    cout<<"请输入第一个数：";
+    cin>>a;
+    cout<<"请输入第二个数：";
+    cin>>b;
+    sum=a+b;
+    diff=a-b;
+    product=a*b;
+    quotient=static_cast<double>(a)/b;
+    remainder=a%b;
+    cout<<a<<"+"<<b<<"="<<sum<<endl;
+    cout<<a<<"-"<<b<<"="<<diff<<endl;
+    cout<<a<<"*"<<b<<"="<<product<<endl;
+    cout<<a<<"/"<<b<<"="<<quotient<<endl;
+    cout<<a<<"%"<<b<<"="<<remainder<<endl;
+    return 0;
+}
