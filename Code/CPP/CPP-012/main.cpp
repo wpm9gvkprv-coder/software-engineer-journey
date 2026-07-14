b45b036298b0a406491de073a7b495f376e35b41
WPM9gvkprv-coder <wpm9gvkprv@privaterelay.appleid.com>
2026-07-14 11:59:23 +0800

    指针基础-地址解引用，指针参数交换

---
+++ Code/CPP/CPP-012/main.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+void swapwithPOINTER(int *p1, int *p2) {
+   
+    int temp;
+    temp=*p1;
+    *p1=*p2;
+    *p2=temp;
+}
+
+int main() {
+    int x = 5, y = 10;
+    int *p1=&x;
+    int *p2=&y;
+    swapwithPOINTER(p1,p2);
+    std::cout << "x = " << x << ", y = " << y << std::endl;
+}
+
+
+
+
